46ef8eeed954b0f6e711f0d2c62b5c3ab85517b6
qianny409-lgtm <qianny409@gmail.com>
2025-10-26 17:57:24 +0800

    Add User model with password hashing

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "student", "parent"], default: "student" }
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+export default mongoose.model("User", userSchema);
